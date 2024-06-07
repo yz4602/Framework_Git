8fe6587508126b7758a06f3d0f7e164f853a2136
Mark_Z <144499373+yz4602@users.noreply.github.com>
2024-06-07 08:46:25 +0800

    Add FSM

---
+++ Assets/FSM/StateMachine/FSM.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StateType
+{
+    Idle, Patrol, Attack, Death
+}
+
+[Serializable]
+public class Parameter
+{
+    public int health;
+    public float moveSpeed;
+    public float idleTime;
+    public Transform[] patrolPoints;
+    public Transform target;
+    public LayerMask targetLayer;
+    public Transform attackPoint;
+    public float attackArea;
+    public Animator animator;
+}
+public class FSM : MonoBehaviour
+{
+    private IState currentState;
+    private Dictionary<StateType, IState> states = new Dictionary<StateType, IState>();
+
+    public Parameter parameter;
+
+    void Start()
+    {
+        states.Add(StateType.Idle, new IdleState(this));
+        states.Add(StateType.Patrol, new PatrolState(this));
+        states.Add(StateType.Attack, new AttackState(this));
+        states.Add(StateType.Death, new DeathState(this));
+
+        TransitionState(StateType.Idle);
+
+        parameter.animator = transform.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        currentState.OnUpdate();
+    }
+
+    public void TransitionState(StateType type)
+    {
+        if (currentState != null)
+            currentState.OnExit();
+        currentState = states[type];
+        currentState.OnEnter();
+    }
+
+    public void FlipTo(Transform target)
+    {
+        if (target != null)
+        {
+            if (transform.position.x > target.position.x)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else if (transform.position.x < target.position.x)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            parameter.target = other.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            parameter.target = null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(parameter.attackPoint.position, parameter.attackArea);
+    }
+}
---
+++ Assets/FSM/StateMachine/IdleState.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : IState
+{
+    private FSM manager;
+    private Parameter parameter;
+
+    private float timer;
+    public IdleState(FSM manager)
+    {
+        this.manager = manager;
+        this.parameter = manager.parameter;
+    }
+    public void OnEnter()
+    {
+        parameter.animator.Play("Idle");
+    }
+
+    public void OnUpdate()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= parameter.idleTime)
+        {
+            manager.TransitionState(StateType.Patrol);
+        }
+    }
+
+    public void OnExit()
+    {
+        timer = 0;
+    }
+}
+
+public class PatrolState : IState
+{
+    private FSM manager;
+    private Parameter parameter;
+
+    private int patrolPosition;
+    public PatrolState(FSM manager)
+    {
+        this.manager = manager;
+        this.parameter = manager.parameter;
+    }
+    public void OnEnter()
+    {
+        parameter.animator.Play("Walk");
+    }
+
+    public void OnUpdate()
+    {
+        manager.FlipTo(parameter.patrolPoints[patrolPosition]);
+
+        manager.transform.position = Vector2.MoveTowards(manager.transform.position,
+            parameter.patrolPoints[patrolPosition].position, parameter.moveSpeed * Time.deltaTime);
+
+        if (parameter.target !=  null && Vector2.Distance(parameter.attackPoint.position, parameter.target.position) < parameter.attackArea)
+        {
+            patrolPosition--;
+            manager.TransitionState(StateType.Attack);
+        }
+
+        if (Vector2.Distance(manager.transform.position, parameter.patrolPoints[patrolPosition].position) < .1f)
+        {
+            manager.TransitionState(StateType.Idle);
+        }
+    }
+
+    public void OnExit()
+    {
+        patrolPosition++;
+
+        if (patrolPosition >= parameter.patrolPoints.Length)
+        {
+            patrolPosition = 0;
+        }
+    }
+}
+
+public class AttackState : IState
+{
+    private FSM manager;
+    private Parameter parameter;
+
+    private AnimatorStateInfo info;
+    public AttackState(FSM manager)
+    {
+        this.manager = manager;
+        this.parameter = manager.parameter;
+    }
+    public void OnEnter()
+    {
+        parameter.animator.Play("Attack");
+    }
+
+    public void OnUpdate()
+    {
+        if(Vector2.Distance(parameter.attackPoint.position, parameter.target.position) > parameter.attackArea)
+        {
+            manager.TransitionState(StateType.Patrol);
+        }
+    }
+
+    public void OnExit()
+    {
+
+    }
+}
+
+public class DeathState : IState
+{
+    private FSM manager;
+    private Parameter parameter;
+
+    public DeathState(FSM manager)
+    {
+        this.manager = manager;
+        this.parameter = manager.parameter;
+    }
+    public void OnEnter()
+    {
+        parameter.animator.Play("Dead");
+    }
+
+    public void OnUpdate()
+    {
+
+    }
+
+    public void OnExit()
+    {
+
+    }
+}
